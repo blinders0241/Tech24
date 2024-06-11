8658286c2159ae8c592a09ee726cfb060fa0c9ad
Ahmed Ali SIDDIQUE <peaky.blinders0241@gmail.com>
2024-06-11 23:02:38 +0530

    updated path

--- drfcore/FarFlix/FFlibs/Globster.py
+++ drfcore/FarFlix/FFlibs/Globster.py
@@ -9,6 +9,6 @@ class Globster():
     def getSQLconnection(self):
         SQL_URL = r'C:\SIMPLY_Official\2024\TechHome241\drfcore'
         
-    SQL_URL = r'C:\SIMPLY_Official\2024\TechHome241\drfcore'
-    SavePath = r"C:\SIMPLY_Official\2024\TechHome241\drfcore\FarFlix\data\\"
-    SaveMovieDetailsIMDB = r"C:\SIMPLY_Official\2024\TechHome241\drfcore\FarFlix\data\MovieDetailsFromIMDB\\"
+    SQL_URL = r'C:\SIMPLY_Official\2024\TechHome24\drfcore'
+    SavePath = r"C:\SIMPLY_Official\2024\TechHome24\drfcore\FarFlix\data\\"
+    SaveMovieDetailsIMDB = r"C:\SIMPLY_Official\2024\TechHome24\drfcore\FarFlix\data\MovieDetailsFromIMDB\\"
